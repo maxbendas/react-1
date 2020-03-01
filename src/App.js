91444d8870b3aab45c7bb1c88931958a83e59509
Max <maxbendas86@gmail.com>
2020-03-01 23:08:38 +0200

    one

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
+
 
 const App = () => {
   return (
     <div>
-      <div>one div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
-    </div>    
+      <Header />
+      <Technologies />
+    </div>
   );
 }
 
