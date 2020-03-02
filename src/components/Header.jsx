d69cc4325b76c9c234ce0afbb0e8c3cf7e6fe9be
Max <maxbendas86@gmail.com>
2020-03-02 18:53:25 +0200

    lesson-12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://image.shutterstock.com/image-vector/max-logo-vector-260nw-520775092.jpg' />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://cdn.beach-inspector.com/static/awards/lp-header.jpg?w=1200&h=400&fit=crop" />
+      </div>
+      <div>
+        <img src="https://www.catster.com/wp-content/uploads/2017/11/A-Siamese-cat.jpg" />
+        descr
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+        </div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Profile;
